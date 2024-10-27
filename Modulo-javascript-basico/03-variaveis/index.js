515395e6d2810bc47372f76951c41eb3a3417686
Arthur Moraes <arthur.m.thurler@gmail.com>
2024-10-27 20:09:14 -0300

    .

---
+++ Modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,4 @@
+var pessoa = "Arthur"
+const emailPessoa = "arthur@gmail.com"
+emailPessoa = "arthur@hotmail.com"
+console.log(emailPessoa)
---
+++ Modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,5 @@
+let nome = "Arthur" //primitivo string (texto) 
+let numero = 123 //primitivo number 
+let usuarioVerificado = false //primitivo booleano 
+let pessoa = null //primitivo null 
+let telefone = undefined //primitivo undefined 
